e04744ba7626b531c4ad83bf91bc6024510f0b09
Brittany Walentin <brittw@uber.com>
2016-06-07 20:26:20 -0700

    Adds closure example.

---
+++ closures.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "fmt"
+)
+
+func intSeq() func() int {
+  i := 0
+  // return value of intSeq() method is a closure, an anonymous fxn
+  return func() int {
+    if i == 0 {
+      i += 1
+    } else {
+      i += i
+    }
+    return i
+  }
+}
+
+func main() {
+  myNextInt := intSeq()
+  fmt.Println(myNextInt())
+  fmt.Println(myNextInt())
+  fmt.Println(myNextInt())
+  fmt.Println(myNextInt())
+
+}
