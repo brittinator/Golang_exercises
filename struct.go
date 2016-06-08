9eb41d5f550f9bb59215a8af025ea09150b2bd09
Brittany Walentin <brittw@uber.com>
2016-06-08 08:59:03 -0700

    Adds struct example.

---
+++ struct.go
@@ -0,0 +1,40 @@
+/*
+Go’s structs are typed collections of fields.
+They’re useful for grouping data together to form records
+*/
+
+package main
+
+import "fmt"
+
+
+type wizard struct{
+  name string
+  house string
+  good bool
+}
+
+func main(){
+  hp := wizard{
+    name: "Harry Potter",
+    house: "Gryffindor",
+    good: true,
+  }
+
+  tr := wizard {
+    name: "Tom Riddle",
+    house: "Slytherin",
+    good: true,
+}
+  fmt.Println(hp, tr)
+
+  fmt.Println("House ", hp.house)
+
+  // to reassign things, need to get the pointer for the object first
+  trPointer := &tr
+  trPointer.name = "Voldemort"
+  trPointer.good = false
+
+  fmt.Println(tr)
+
+}
