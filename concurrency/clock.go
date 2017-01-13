305db6a02f1385709276e315fd0fc3accb051143
Brittany Walentin <brittw@uber.com>
2017-01-12 16:39:35 -0800

    Adds clock server go example to exhibit go's concurrency model with go routines.

---
+++ concurrency/clock.go
@@ -0,0 +1,43 @@
+/* Clock implements a clock server with go routines,
+which enables it to serve multiple requests to it.
+
+Usage:
+go run clock.go
+nc localhost 3000 (try this on multiple tabs)
+*/
+
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	// infinite loop
+	for {
+		if _, err := io.WriteString(c, time.Now().Format("15:03:05\n")); err != nil {
+			// this means the client disconnected
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
