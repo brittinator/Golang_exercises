787b322a0296b7db337517d5c18dc6b5712911a3
Brittany Walentin <brittw@uber.com>
2017-02-09 19:28:09 -0800

    WIP

---
+++ concurrency/context/README.md
@@ -0,0 +1,4 @@
+# Using go's context functionality when dealing with concurrency
+
+This work is based heavily off of this blog post: http://dahernan.github.io/2015/02/04/context-and-cancellation-of-goroutines/
+Also a good StackOverflow answer to 'The term "Context" in programming': http://stackoverflow.com/questions/6145091/the-term-context-in-programming
---
+++ concurrency/context/server.go
@@ -0,0 +1,31 @@
+package main
+
+// This a randomly slow/fast server
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+// RandomlySlowServer is sometimes fast, and other times
+// much slower
+func RandomlySlowServer(w http.ResponseWriter, req *http.Request) {
+	fiftyFiftyShot := rand.Intn(2)
+	fmt.Println(fiftyFiftyShot)
+
+	if fiftyFiftyShot == 0 {
+		time.Sleep(6 * time.Second)
+		fmt.Fprintf(w, "GO sloooowly %v\n", fiftyFiftyShot)
+		fmt.Printf("GO sloooowly %v\n", fiftyFiftyShot)
+		return
+	}
+	fmt.Fprintf(w, "GO quickly %v\n", fiftyFiftyShot)
+	fmt.Printf("GO quickly %v\n", fiftyFiftyShot)
+	return
+}
+
+func main() {
+	http.HandleFunc("/", RandomlySlowServer)
+	http.ListenAndServe(":1234", nil)
+}
